b06a9ec2092601c14ceb1d3cac3717fa31cf440f
Michael Sissons <michaelglsissons@gmail.com>
2026-05-11 13:20:40 -0400

    files and stuff

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,4 +10,31 @@ public class GameData : ScriptableObject
     public List<StructureData> structures;
     public List<ResourceData> resources;
 
+    public TextAsset unitTable;
+    public TextAsset structureTable;
+    public TextAsset resourceTable;
+
+
+    public List<UnitData> GetUnitsFromTable()
+    {
+        List<UnitData> unitsTable = new List<UnitData>();
+
+        //Split up data from csv
+        string[] data = unitTable.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
+
+        //Divide up data into sets
+        int tableSize = data.Length / NumberOfColumns - 1;
+        myUnitDataList.unitData = new UnitData[tableSize];
+
+        for (int i = 0; i < tableSize; i++)
+        {
+            myUnitDataList.unitData[i] = new UnitData();
+            myUnitDataList.unitData[i].name = data[NumberOfColumns * (i + 1)];
+            myUnitDataList.unitData[i].maxHealth = float.Parse(data[NumberOfColumns * (i + 1) + 1]);
+            myUnitDataList.unitData[i].attackDamage = float.Parse(data[NumberOfColumns * (i + 1) + 2]);
+            myUnitDataList.unitData[i].attackRange = float.Parse(data[NumberOfColumns * (i + 1) + 3]);
+            myUnitDataList.unitData[i].attackSpeed = float.Parse(data[NumberOfColumns * (i + 1) + 4]);
+            myUnitDataList.unitData[i].moveSpeed = float.Parse(data[NumberOfColumns * (i + 1) + 5]);
+        }
+    }
 }
